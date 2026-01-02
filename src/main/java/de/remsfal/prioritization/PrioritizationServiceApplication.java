4f578baddd1735f83b973571bf3841d51b78ce58
MohammedHammouri03 <mhh.hammouri@gmail.com>
2026-01-02 17:27:57 +0100

    spring boot integration

---
+++ src/main/java/de/remsfal/prioritization/PrioritizationServiceApplication.java
@@ -0,0 +1,13 @@
+package de.remsfal.prioritization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrioritizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrioritizationServiceApplication.class, args);
+	}
+
+}
